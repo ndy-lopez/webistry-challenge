4909009a89f68fad6c5c83a3571cc4adf7fc99ee
Andy Lopez <ndy.loredo@gmail.com>
2023-06-23 13:49:19 -0400

    Logics of validations and need to work on conditionals

--- main.js
+++ main.js
@@ -3,30 +3,22 @@ const validEmail = (email) => {
   return regex.test(email);
 };
 
-// console.log(validEmail('test@email.com')); // => true
-
 const checked = (checkbox) => {
-  return checkbox.checked
-}
+  return checkbox.checked;
+};
 
-// const myCheckbox = document.getElementById("myCheckbox");
-// console.log(checked (myCheckbox)); => true when checked, false otherwise
+const form = document.querySelector('#form');
+const emailInput = document.querySelector('#emailValidation');
+const emailValidationMessage = document.querySelector('#emailValidationMessage');
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailValue = emailInput.value;
 
-const form = document.querySelector('#form')
-const emailValidationText = document.querySelector('#emailValidation');
-const checkBoxValidationText = document.querySelector('#checkBoxValidationText');
-
-console.log(form)
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault()
-//   const emailValue = document.querySelector('#email').value
-//   const checkbox = document.querySelector('#checkbox')
-//   console.log(checkbox)
-
-//   if (validEmail === false && checked(checkbox)) {
-//     emailValidationText.classList.remove("display-none")
-//   } else if (checked(checkbox ))
-
-// })
+  if (validEmail === false) {
+    emailValidationMessage.classList.remove("display-none");
+  } else {
+    emailValidationMessage.classList.add("display-none");
+    // Perform other validation or submission logic
+  }
+});
